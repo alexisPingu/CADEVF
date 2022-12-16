0a84915c3564f78e693d8dd1dbb92473627f89f2
alexisPingu <alexisurielmartinezcruz385@gmail.com>
2022-12-15 18:57:13 -0600

    Cambio de nombres y validacion de formulario actualizar

--- app/src/main/java/com/beyondthecode/cade/fragments/ActualizarFragment.kt
+++ app/src/main/java/com/beyondthecode/cade/fragments/ActualizarFragment.kt
@@ -1,12 +1,14 @@
 package com.beyondthecode.cade.fragments
 
 import android.content.Intent
+import android.opengl.ETC1.isValid
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Toast
 import androidx.appcompat.app.AlertDialog
+import androidx.appcompat.app.AppCompatActivity
 import androidx.fragment.app.Fragment
 import com.beyondthecode.cade.R
 import com.beyondthecode.cade.api.modelos.AlumnoDto
@@ -34,57 +36,75 @@ class ActualizarFragment : Fragment() {
         actualizarDatos()
 
 
-        binding?.btnActualizar!!.setOnClickListener { view ->
-            var alumnoUpdateDto: AlumnoUpdateDto = AlumnoUpdateDto(
-                binding?.actEmail!!.text.toString(),
-                binding?.actContraseA!!.text.toString(),
-                binding?.actTelefono!!.text.toString(),
-                binding?.actDireccion!!.text.toString(),
-            )
-            var service = ApiClientt.getRetrofitInstance()?.create(AlumnoRepository::class.java)
-            service?.updateAlumn(alumnoUpdateDto, AlumnoOBJ.alumno!!.id)
-                ?.enqueue(object : Callback<AlumnoDto?> {
-                    override fun onResponse(
-                        call: Call<AlumnoDto?>,
-                        response: Response<AlumnoDto?>
-                    ) {
-                        Toast.makeText(
-                            binding?.root!!.context,
-                            "actualizado... ",
-                            Toast.LENGTH_SHORT
-                        ).show()
-                        AlumnoOBJ.alumno = response.body()
-                        activity?.onBackPressed();
+        binding?.btnActualizar!!.setOnClickListener {view ->
 
-                        val view =
-                            View.inflate(binding!!.root.context, R.layout.alerta_actualizado, null)
-                        val builder = AlertDialog.Builder(binding!!.root.context)
-                        builder.setView(view)
+            if(isValid()){
 
-                        val dialog = builder.create()
-                        dialog.show()
-                        dialog.window?.setBackgroundDrawableResource(android.R.color.transparent)
-                        Toast.makeText(
-                            binding?.root!!.context,
-                            "informacion del " +
-                                    "\n ${response.body()?.id} actualizado correctamente ",
-                            Toast.LENGTH_SHORT
-                        ).show()
-                    }
+                var alumnoUpdateDto: AlumnoUpdateDto = AlumnoUpdateDto(
+                    binding?.actEmail!!.text.toString(),
+                    binding?.actContraseA!!.text.toString(),
+                    binding?.actTelefono!!.text.toString(),
+                    binding?.actDireccion!!.text.toString(),
+                )
+                var service = ApiClientt.getRetrofitInstance()?.create(AlumnoRepository::class.java)
+                service?.updateAlumn(alumnoUpdateDto, AlumnoOBJ.alumno!!.id)
+                    ?.enqueue(object : Callback<AlumnoDto?> {
+                        override fun onResponse(
+                            call: Call<AlumnoDto?>,
+                            response: Response<AlumnoDto?>
+                        ) {
+                            Toast.makeText(
+                                binding?.root!!.context,
+                                "actualizado... ",
+                                Toast.LENGTH_SHORT
+                            ).show()
+                            AlumnoOBJ.alumno = response.body()
+                            activity?.onBackPressed();
 
-                    override fun onFailure(call: Call<AlumnoDto?>, t: Throwable) {
-                        Toast.makeText(
-                            binding?.root!!.context,
-                            "no se pudo registrar, pero si... ",
-                            Toast.LENGTH_LONG
-                        ).show()
-                        startActivity(Intent(binding?.root!!.context, Matricula::class.java))
-                    }
+                            val view =
+                                View.inflate(binding!!.root.context, R.layout.alerta_actualizado, null)
+                            val builder = AlertDialog.Builder(binding!!.root.context)
+                            builder.setView(view)
+
+                            val dialog = builder.create()
+                            dialog.show()
+                            dialog.window?.setBackgroundDrawableResource(android.R.color.transparent)
+                            Toast.makeText(
+                                binding?.root!!.context,
+                                "informacion del " +
+                                        "\n ${response.body()?.id} actualizado correctamente ",
+                                Toast.LENGTH_SHORT
+                            ).show()
+                        }
+
+                        override fun onFailure(call: Call<AlumnoDto?>, t: Throwable) {
+                            Toast.makeText(
+                                binding?.root!!.context,
+                                "no se pudo registrar, pero si... ",
+                                Toast.LENGTH_LONG
+                            ).show()
+                            startActivity(Intent(binding?.root!!.context, Matricula::class.java))
+                        }
 
 
-                })
+                    })
+            }else{
+                Toast.makeText( binding?.root!!.context, "Faltan datos", Toast.LENGTH_SHORT).show()
+            }
         }
         return binding!!.root
+    }
+
+    private fun isValid(): Boolean {
+        var email=binding?.actEmail!!.text.toString()
+        var contraseña=binding?.actContraseA!!.text.toString()
+        var telefono=binding?.actTelefono!!.text.toString()
+        var direcccion=binding?.actDireccion!!.text.toString()
+        if(email.isNotEmpty() && contraseña.isNotEmpty() && telefono.isNotEmpty() && direcccion.isNotEmpty()){
+            return true
+        }else{
+            return false
+        }
     }
 
     private fun actualizarDatos() {
